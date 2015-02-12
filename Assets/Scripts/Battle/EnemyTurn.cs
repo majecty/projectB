fee7599fbe9f33a920c6751d3b98153e19319704
ParkJuhyeong <majecty@gmail.com>
2015-02-12 22:49:20 +0900

    Make default turn structure.

---
+++ Assets/Scripts/Battle/EnemyTurn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Battle
+{
+    public class EnemyTurn : Turn
+    {
+        public override Run StartTurn()
+        {
+            return Run.After (1.0f, () => Debug.Log ("Enemy turn end."));
+        }
+    }
+}
---
+++ Assets/Scripts/Battle/PlayerTurn.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Battle
+{
+    public class PlayerTurn : Turn
+    {
+        public override Run StartTurn()
+        {
+            return Run.After (1.0f, () => Debug.Log ("Player turn end."));
+        }
+    }
+}

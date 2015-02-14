5e653b11fcc68add5d53959a8fc850ad02441f70
ParkJuhyeong <majecty@gmail.com>
2015-02-14 13:26:00 +0900

    Add core data structures.

---
+++ Assets/Scripts/Battle/CoreData/CoreDatas.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Battle.CoreData
+{
+    public class Player
+    {
+        private int hp = 100;
+        public int Hp { get { return hp; } }
+    }
+
+    public class Enemy
+    {
+        private int hp = 100;
+        public int Hp { get { return hp; } }
+
+        public void DiminishLife(float damage)
+        {
+            hp -= (int)damage;
+        }
+    }
+
+    public class Deck
+    {
+        private List<Card> cards = new List<Card>();
+        public Card GetCard(int index)
+        {
+            return cards[index];
+        }
+    }
+
+    public class Card
+    {
+        private Stat stat;
+        public Stat Stat { get { return stat; } }
+    }
+
+    public struct Stat
+    {
+        public readonly float Attack;
+        public readonly float Defense;
+        public readonly float Speed;
+
+        public Stat(float attack, float defense, float speed)
+        {
+            Attack = attack;
+            Defense = defense;
+            Speed = speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Battle/State.cs
@@ -0,0 +1,24 @@
+﻿using Battle.CoreData;
+
+namespace Battle
+{
+    public class State
+    {
+        public readonly Player Player;
+        public readonly Enemy Enemy;
+
+        public State()
+        {
+            Player = new Player();
+            Enemy = new Enemy();
+        }
+
+        public override string ToString()
+        {
+            var log = "Player: " + Player.Hp;
+            log += "\nEnemy: " + Enemy.Hp;
+
+            return log;
+        }
+    }
+}

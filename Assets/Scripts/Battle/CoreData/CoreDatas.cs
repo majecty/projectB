7c734ad76abd66b0badf1bf0de957fce8e3c88b2
ParkJuhyeong <majecty@gmail.com>
2015-02-14 13:26:00 +0900

    Add core data structures.

---
+++ Assets/Scripts/Battle/CoreData/CoreDatas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Battle.CoreData
+{
+    public class Player
+    {
+        private int hp = 100;
+        public int Hp { get { return hp; } }
+    }
+
+    public class Enemy
+    {
+        private int hp = 100;
+        public int Hp { get { return hp; } }
+
+        public void DiminishLife(float damage)
+        {
+            hp -= (int)damage;
+        }
+    }
+}

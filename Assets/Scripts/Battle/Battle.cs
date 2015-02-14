6a8888f9a285cd20a3b6e57d344f895e29385016
ParkJuhyeong <majecty@gmail.com>
2015-02-14 17:50:49 +0900

    Implement game end.

--- Assets/Scripts/Battle/Battle.cs
+++ Assets/Scripts/Battle/Battle.cs
@@ -7,6 +7,13 @@ namespace Battle
     {
         [SerializeField] private EventReceiver eventReceiver;
 
+        enum TurnEndType
+        {
+            Win,
+            Lose,
+            NotEnd
+        }
+
         private static Battle instance;
         public static Battle Instance { get { return instance; } }
 
@@ -33,7 +40,35 @@ namespace Battle
                 yield return enemyTurnRoutine.WaitFor;
 
                 Debug.Log("Turn total: " + state);
+
+                var turnEndType = EndTurn();
+                if (turnEndType != TurnEndType.NotEnd)
+                {
+                    break;
+                }
             }
+
+            Debug.Log("Game End!");
+        }
+
+        private TurnEndType EndTurn()
+        {
+            TurnEndType endType;
+            if (state.Player.Hp <= 0)
+            {
+                endType = TurnEndType.Lose;
+                Debug.Log("Lose");
+            }
+            else if (state.Enemy.Hp <= 0)
+            {
+                endType = TurnEndType.Win;
+                Debug.Log("Win");
+            }
+            else
+            {
+                endType = TurnEndType.NotEnd;
+            }
+            return endType;
         }
     }
 }

375a0ba4089c6966650782d8a7f01e45f9a326e2
ParkJuhyeong <majecty@gmail.com>
2015-02-15 11:29:49 +0900

    Add enumerable debug print function.
    List's default ToString method does not print it's element.

---
+++ Assets/Libraries/DebugPrint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public static class DebugExtension
+{
+    public static string DebugToString<T>(this IEnumerable<T> _enumerable)
+    {
+        var _log = "";
+        foreach (var element in _enumerable)
+        {
+            _log += element.ToString();
+            _log += ", ";
+        }
+
+        return _log;
+    }
+}

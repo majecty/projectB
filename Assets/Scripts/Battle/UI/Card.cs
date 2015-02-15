b19e56b0472aed9c4f05f37eba53eb8ea70c0d07
ParkJuhyeong <majecty@gmail.com>
2015-02-15 10:33:34 +0900

    Show card clicked ui.

---
+++ Assets/Scripts/Battle/UI/Card.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Battle.UI
+{
+    [RequireComponent (typeof (Button))]
+    public class Card : MonoBehaviour
+    {
+        [SerializeField] private Image selectedImage;
+        [SerializeField] private int cardIndex;
+        private Button mButton;
+
+        private void Start()
+        {
+            mButton = GetComponent<Button>();
+            mButton.onClick.AddListener(() => Battle.Instance.EventReceiver.OnClickCard(cardIndex));
+        }
+
+        private void Update()
+        {
+            var _state = Battle.Instance.State;
+            var _isClicked = _state.player.IsClickedCard(cardIndex);
+            selectedImage.gameObject.SetActive(_isClicked);
+        }
+    }
+}
